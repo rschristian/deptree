ce0be7ef19b946c0e9561f1ee55b56165fb1fe85
Nestor Vera <nestorvera@me.com>
2024-11-07 15:38:32 +0100

    feat: Allow package tree to be collapsed

--- src/components/PackageTree.jsx
+++ src/components/PackageTree.jsx
@@ -1,11 +1,15 @@
+import { useState } from 'preact/hooks';
+
 /**
  * @param {Object} props
  * @param {import('../pkg/types.d.ts').ModuleTree} props.pkg
  * @param {number} [props.depth=0]
  * @param {boolean} [props.isLast=false]
- * @param {string} [props.prefix='']
+ * @param {string} [props.prefix='  ']
  */
-export function PackageTree({ pkg, depth = 0, isLast = false, prefix = '' }) {
+export function PackageTree({ pkg, depth = 0, isLast = false, prefix = '  ' }) {
+    const [isVisible, setIsVisible] = useState(true);
+
     let lineSymbol = prefix;
     let childPrefix = prefix;
     if (depth > 0) {
@@ -17,12 +21,31 @@ export function PackageTree({ pkg, depth = 0, isLast = false, prefix = '' }) {
         ? `underline(& offset-4) decoration(2 replacement-${pkg.replacement.type}(& dark:dark))`
         : '';
 
+    function handleTreeVisibility() {
+        setIsVisible(!isVisible);
+    }
+
     return (
-        <div class={(depth == 0 && 'mb-4 last:mb-2') || (depth == 1 && 'ml-4')}>
-            <pre class="w-max">
+        <div class={[depth == 0 ? 'mb-4 last:mb-2' : '', 'grid _gap-0.5 text-sm']}>
+            <pre class={[
+                'flex items-center w-full',
+                !isVisible ? 'collapse-tree' : '',
+                'p-px hover:bg-highlightContent/20 dark:hover:bg-highlightContent-dark/20) rounded',
+            ]}>
+                <button
+                    class={[
+                        'flex justify-center items-center w-6 h-6',
+                        'bg-highlight(& dark:dark) drop-shadow-lg rounded hocus:(opacity-80 outline(1 & primary))',
+                        'mr-1 text-xs leading-none',
+                        pkg.dependencies?.length ? '' : 'invisible',
+                    ]}
+                    onClick={handleTreeVisibility}
+                >
+                    {isVisible ? '-' : '+'}
+                </button>
                 {lineSymbol}
                 <a
-                    class={`px-1 py-0.5 text-highlightContent(& dark:dark) bg-highlight(& dark:dark) rounded hocus:(opacity-80 outline(1 & primary)) ${decoration}`}
+                    class={`px-1 py-1 leading-none text-highlightContent(& dark:dark) bg-highlight(& dark:dark) rounded hocus:(opacity-80 outline(1 & primary)) ${decoration}`}
                     href={`https://npm.im/${pkg.name}`}
                     target="_blank"
                     rel="noopener noreferrer"
