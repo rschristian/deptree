a0d3a61353e4e3afc3ffdb235d6a6672eabd0035
Nestor Vera <nestorvera@me.com>
2024-11-07 16:52:50 +0100

    feat: Allow users to filter tree by replacement type

--- src/components/DataBox.jsx
+++ src/components/DataBox.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'preact/hooks';
+import { useEffect, useRef, useState } from 'preact/hooks';
 import Hint from 'preact-hint';
 
 import { PackageTree } from './PackageTree.jsx';
@@ -36,35 +36,39 @@ export function DataBox({ queryResult }) {
         container.current.scrollLeft = scrollLeft - scroll;
     };
 
+    const [isNativeActive, setIsNativeActive] = useState(false);
+    const [isMicroActive, setIsMicroActive] = useState(false);
+
     return (
         <>
+            {!queryResult.error && (
+                <div class="z-10 sticky top-8 bg-card(& dark:dark) max-w-4xl mx-auto px-2 py-0 md:px-6 md:py-4 border(& resultBorder 1) rounded">
+                    <div class="grid gap-y-1 gap-x-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 items-end [&>*]:p-1 [&>*]:text-left [&>*]:leading-none">
+                        <div class={['grid grid-cols-[1fr_auto] md:grid-cols-1 gap-1 items-baseline rounded', (isNativeActive || isMicroActive) ? 'opacity-50' : 'opacity-100']}>
+                            <p class="">Total Nodes</p>
+                            <p class="text-xl md:text-4xl font-semibold">{queryResult.stats.nodeCount}</p>
+                        </div>
+                        <div class={['grid grid-cols-[1fr_auto] md:grid-cols-1 gap-1 items-baseline rounded', (isNativeActive || isMicroActive) ? 'opacity-50' : 'opacity-100']}>
+                            <p class="">Total Modules</p>
+                            <p class="text-xl md:text-4xl font-semibold">{queryResult.stats.moduleCount}</p>
+                        </div>
+                        <button class={['grid grid-cols-[1fr_auto] md:grid-cols-1 gap-1 items-baseline rounded hocus:(opacity-80 outline(2 & primary))', (isNativeActive || !(isNativeActive || isMicroActive)) ? 'opacity-100' : 'opacity-50']} onClick={() => setIsNativeActive(!isNativeActive)}>
+                            <p class="">Native Replacement</p>
+                            <p class="text-xl md:text-4xl font-semibold text-replacement-native(& dark:dark)">{queryResult.stats.nativeCount}</p>
+                        </button>
+                        <button class={['grid grid-cols-[1fr_auto] md:grid-cols-1 gap-1 items-baseline rounded hocus:(opacity-80 outline(2 & primary))', (isMicroActive || !(isNativeActive || isMicroActive)) ? 'opacity-100' : 'opacity-50']} onClick={() => setIsMicroActive(!isMicroActive)}>
+                            <p class="">Micro Utilities</p>
+                            <p class="text-xl md:text-4xl font-semibold text-replacement-micro(& dark:dark)">{queryResult.stats.microCount}</p>
+                        </button>
+                    </div>
+                </div>
+            )}
+
             <section
                 class={`relative mt-8 p-4 border(& ${
                     queryResult.error ? 'red' : 'resultBorder'
                 } 1) rounded`}
             >
-                {!queryResult.error && (
-                    <>
-                        <Hint
-                            template={() => (
-                                <div class="text-left">
-                                    Module Count: {queryResult.stats.moduleCount}
-                                    <br />
-                                    Modules with Native Replacement: {queryResult.stats.nativeCount}
-                                    <br />
-                                    Micro Utility Modules: {queryResult.stats.microCount}
-                                    <br />
-                                    Total Number of Nodes: {queryResult.stats.nodeCount}
-                                </div>
-                            )}
-                        >
-                            <svg tabindex={0} data-hint=" " class="absolute right-0 z-50">
-                                <use href="/assets/icons.svg#info" />
-                            </svg>
-                        </Hint>
-                        <Key />
-                    </>
-                )}
                 <Hint
                     template={(content) => (
                         <p>
@@ -74,7 +78,12 @@ export function DataBox({ queryResult }) {
                 >
                     <div
                         ref={container}
-                        class="overflow-x-auto p-0.5"
+                        class={[
+                            'overflow-x-auto p-0.5',
+                            (isNativeActive && isMicroActive) ? 'collapse-tree-all'
+                                : isNativeActive ? 'collapse-tree-native'
+                                : isMicroActive ? 'collapse-tree-micro' : '',
+                        ]}
                         {...(!queryResult.error && {
                             onMouseMove: move,
                             onMouseDown: startDragging,
