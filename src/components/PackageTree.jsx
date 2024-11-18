f7b8c51151fca699dd1e7a0c031bfc24b1d83fcd
Ryan Christian <rchristian@ryanchristian.dev>
2024-11-18 16:23:30 -0600

    feat: Simple collapse of subtrees
    Co-authored-by: Nestor Vera <nestorvera@me.com>

--- src/components/PackageTree.jsx
+++ src/components/PackageTree.jsx
@@ -1,3 +1,5 @@
+import { useRef } from 'preact/hooks';
+
 /**
  * @param {Object} props
  * @param {import('../pkg/types.d.ts').ModuleTree} props.pkg
@@ -6,20 +8,40 @@
  * @param {string} [props.prefix='']
  */
 export function PackageTree({ pkg, depth = 0, isLast = false, prefix = '' }) {
-    let lineSymbol = prefix;
-    let childPrefix = prefix;
+    const ref = useRef(null);
+
+    let lineSymbol = prefix,
+        childPrefix = prefix;
     if (depth > 0) {
         lineSymbol += (isLast ? '└' : '├').padEnd(6, '-');
         childPrefix += (isLast ? ' ' : '│').padEnd(8, ' ');
+    } else {
+        childPrefix = ' ';
     }
 
     const decoration = pkg.replacement
         ? `underline(& offset-4) decoration(2 replacement-${pkg.replacement.type}(& dark:dark))`
         : '';
 
+    const hasChildren = pkg.dependencies?.length > 0;
+
+    // Avoid component rerenders as the tree could be quite large
+    const toggleCollapse = () => {
+        if (ref.current) {
+            ref.current.classList.toggle('collapse-tree');
+            ref.current.firstChild.textContent = ref.current.firstChild.textContent === '+' ? '-' : '+';
+            ref.current.firstChild.classList.toggle('text-primary');
+        }
+    };
+
     return (
-        <div class={(depth == 0 && 'mb-4 last:mb-2') || (depth == 1 && 'ml-4')}>
-            <pre class="w-max">
+        <div class={(depth == 0 && 'mb-4 last:mb-2')}>
+            <pre class="w-full text-left" ref={ref}>
+                <button
+                    class={`h-5 w-5 mr-2 bg-highlight(& dark:dark) font-bold hocus:(outline(1 & primary)) rounded ${hasChildren ? '' : 'invisible'}`}
+                    onClick={toggleCollapse}>
+                    -
+                </button>
                 {lineSymbol}
                 <a
                     class={`px-1 py-0.5 text-highlightContent(& dark:dark) bg-highlight(& dark:dark) rounded hocus:(opacity-80 outline(1 & primary)) ${decoration}`}
@@ -31,7 +53,7 @@ export function PackageTree({ pkg, depth = 0, isLast = false, prefix = '' }) {
                     {`${pkg.name}@${pkg.version}`}
                 </a>
             </pre>
-            {pkg.dependencies?.length &&
+            {hasChildren &&
                 pkg.dependencies.map((dep, i) => (
                     <PackageTree
                         pkg={dep}
